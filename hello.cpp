76c7adee9e2723af8bd90b69884c5ddaa1428e85
shaurya2918 <shaurya.negi1991@gmail.com>
2025-09-16 23:48:50 -0700

    Updated hello.cpp with new line

--- hello.cpp
+++ hello.cpp
@@ -11,3 +11,16 @@ int main() {
     }
     std::cout << std::endl;
 }
+#include <iostream>
+#include <ranges>
+#include <vector>
+
+int main() {
+    std::vector<int> nums = {1, 2, 3, 4, 5};
+
+    // Print squares using C++23 views
+    for (int x : nums | std::views::transform([](int n) { return n * n; })) {
+        std::cout << x << " ";
+    }
+    std::cout << std::endl;
+}
