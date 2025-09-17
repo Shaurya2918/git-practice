a89f17626db6364dd2aac352d5cb1e548677119c
shaurya2918 <shaurya.negi1991@gmail.com>
2025-09-17 00:43:19 -0700

    Fix hello.cpp for C++20 ranges and single main

--- .github/workflows/hello.cpp
+++ .github/workflows/hello.cpp
@@ -6,6 +6,7 @@ using namespace std;
 int main() {
     vector<int> nums = {1, 2, 3, 4, 5};
 
+    // Correct usage of C++20 ranges
     for (int x : nums | views::transform([](int n) { return n * n; })) {
         cout << x << " ";
     }
